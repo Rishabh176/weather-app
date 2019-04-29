d7b2ba58b6816899aa2b564bdf203cbc30163696
Rishabh176 <rishabhemail8@gmail.com>
2019-04-29 23:21:30 +0530

    Updates

---
+++ precache-manifest.6af4d10c0112783e4d3bc14c09595a50.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "8d9c99a4f19116c6d931",
+    "url": "/weather-app/static/js/runtime~main.8d9c99a4.js"
+  },
+  {
+    "revision": "2ad3ab8c03f8e988b9bc",
+    "url": "/weather-app/static/js/main.2ad3ab8c.chunk.js"
+  },
+  {
+    "revision": "0a189fb0a516b5725a96",
+    "url": "/weather-app/static/js/2.0a189fb0.chunk.js"
+  },
+  {
+    "revision": "2ad3ab8c03f8e988b9bc",
+    "url": "/weather-app/static/css/main.5acea616.chunk.css"
+  },
+  {
+    "revision": "0a189fb0a516b5725a96",
+    "url": "/weather-app/static/css/2.70ba6355.chunk.css"
+  },
+  {
+    "revision": "ab78273fa72a1c4bf88f63379fec9151",
+    "url": "/weather-app/index.html"
+  }
+];
